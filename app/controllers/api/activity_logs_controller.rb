32b1604ec78d063faadb42424fd6b7a0981f2bb2
Yair Facio <yair.facio11@gmail.com>
2020-02-15 20:49:46 -0600

    Adding scopes for searching through the activitylogs

--- app/controllers/api/activity_logs_controller.rb
+++ app/controllers/api/activity_logs_controller.rb
@@ -1,7 +1,19 @@
 module Api
   class ActivityLogsController < ApplicationController
     def index
-      @activity_logs = ActivityLog.all
+      @activity_logs = ActivityLog.includes(:baby, :activity, :assistant).all
+      @activity_logs = ActivityLog.includes(:baby, :activity, :assistant).all if params[:status].present? && params[:status] === "2"
+      @activity_logs = ActivityLog.by_status_id(params[:status]) if params[:status].present? && params[:status] != "2"
+      @activity_logs = ActivityLog.by_assistant_id(params[:assistant]) if params[:assistant].present?
+      @activity_logs = ActivityLog.by_assistant_id(params[:assistant]) if params[:assistant].present? && params[:status].present? && params[:status] == "2"
+      @activity_logs = ActivityLog.by_baby_id(params[:baby]) if params[:baby].present?
+      @activity_logs = ActivityLog.by_baby_id(params[:baby]) if params[:baby].present? && params[:status].present? && params[:status] == "2"
+
+      @activity_logs = ActivityLog.by_baby_and_status(params[:baby], params[:status]) if params[:baby].present? && params[:status].present? && params[:status] != "2"
+      @activity_logs = ActivityLog.by_baby_and_assistant(params[:baby], params[:assistant]) if params[:baby].present? && params[:assistant].present?
+      @activity_logs = ActivityLog.by_assistant_and_status(params[:assistant], params[:status]) if params[:assistant].present? && params[:status].present? && params[:status] != "2"
+      @activity_logs = ActivityLog.by_all(params[:baby], params[:assistant], params[:status]) if params[:baby].present? && params[:assistant].present? && params[:status].present? && params[:status] != "2"
+      
       render json: {
         status: 200,
         message: "You have successfuly retrieved the list of ActivityLogs.",
--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -10,6 +10,15 @@ class ActivityLog < ApplicationRecord
   validates :stop_time, date: { after: Proc.new { :start_time }, allow_blank: false }, on: :update
   validate :end_date_after_start_date?, on: :update
 
+  scope :by_baby_id, lambda { |baby_id| includes(:baby, :activity, :assistant).where(:baby_id => baby_id)}
+  scope :by_assistant_id, lambda { |assistant_id| includes(:baby, :activity, :assistant).where(:assistant_id => assistant_id)}
+  scope :by_status_id, lambda { |status| includes(:baby, :activity, :assistant).where(:status => status)}
+
+  scope :by_baby_and_status, lambda { |baby_id, status| includes(:baby, :activity, :assistant).by_baby_id(baby_id).where(:status => status)}
+  scope :by_baby_and_assistant, lambda { |baby_id, assistant_id| includes(:baby, :activity, :assistant).by_baby_id(baby_id).where(:assistant_id => assistant_id)}
+  scope :by_assistant_and_status, lambda { |assistant_id, status| includes(:baby, :activity, :assistant).by_assistant_id(assistant_id).where(:status => status)}
+
+  scope :by_all, lambda { |baby_id, assistant_id, status| includes(:baby, :activity, :assistant).by_baby_id(baby_id).by_assistant_id(assistant_id).by_status_id(status)}
 
   def end_date_after_start_date?
     if TimeDifference.between(start_time, stop_time).in_minutes > 60
