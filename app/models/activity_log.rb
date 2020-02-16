235f66e6017dcd18822d3b40595094001f307d18
Yair Facio <yair.facio11@gmail.com>
2020-02-15 20:14:37 -0600

    Adding validations and installing gems

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -2,4 +2,10 @@ class ActivityLog < ApplicationRecord
   belongs_to :baby
   belongs_to :assistant
   belongs_to :activity
+  validates :baby_id, presence: true
+  validates :activity_id, presence: true
+  validates :assistant_id, presence: true
+  validates :start_time, presence: true
+  validates :stop_time, date: { allow_blank: true}, on: :create
+  validates :stop_time, date: { after: Proc.new { :start_time }, allow_blank: false }, on: :update
 end
