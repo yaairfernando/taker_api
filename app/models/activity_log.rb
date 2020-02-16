a502b0e8dd35407792816324f931d8db9562f074
Yair Facio <yair.facio11@gmail.com>
2020-02-15 20:17:24 -0600

    Adding custom validation for start and stop time attributes

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -8,4 +8,12 @@ class ActivityLog < ApplicationRecord
   validates :start_time, presence: true
   validates :stop_time, date: { allow_blank: true}, on: :create
   validates :stop_time, date: { after: Proc.new { :start_time }, allow_blank: false }, on: :update
+  validate :end_date_after_start_date?, on: :update
+
+
+  def end_date_after_start_date?
+    if TimeDifference.between(start_time, stop_time).in_minutes > 60
+      errors.add :stop_time, "The babie's activities can not last more than 60 minutes..."
+    end
+  end
 end
