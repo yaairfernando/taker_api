55e61a41af2c708c29b56dc9deca32d40d679f7e
Yair Facio <yair.facio11@gmail.com>
2020-02-15 20:05:27 -0600

    Setting up associations between models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,5 @@
 class Activity < ApplicationRecord
+  has_many :activity_logs
+  has_many :assistants, through: :activity_logs
+  has_many :babies, through: :activity_logs
 end
--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,2 +1,4 @@
 class Baby < ApplicationRecord
+  has_many :activity_logs
+  has_many :activities, through: :activity_logs
 end
